cc122220cf956085f781507515301d5b5c4238e9
Kaio Fernandes <kaiof242@gmail.com>
2022-10-04 21:47:33 -0300

    Documentação

--- JAVA/AULA04/Funcionario.java
+++ JAVA/AULA04/Funcionario.java
@@ -1,5 +1,5 @@
 package JAVA.AULA04;
-
+// Aqui temos uma classe para pegarmos os dados dos usuarios
 public class Funcionario 
 {
     String nome; 
@@ -7,10 +7,7 @@ public class Funcionario
     double salario;
     Data dataEntrada; 
     String RG;
-    public Funcionario()
-    {
-        dataEntrada = new Data();
-    }
+    //Aqui é para imprimir esses dados do funcionario 
     void DadosFuncionario()
     {
         System.out.println();
@@ -20,8 +17,10 @@ public class Funcionario
         System.out.println("Entrou na empresa " + dataEntrada.dia + "/" + dataEntrada.mes + "/" + dataEntrada.ano);
         System.out.println("RG do funcionario " + RG);
     }
+    // Aqui é para pegarmos os dados do funcionario
     public Funcionario(String nome, String departamento, double salario , int dia , int mes  , int ano, String rG) 
     {
+        //Aqui é para criarmos o tipo data
         dataEntrada = new Data();
         this.nome = nome;
         this.departamento = departamento;
--- JAVA/AULA04/Program.java
+++ JAVA/AULA04/Program.java
@@ -4,14 +4,17 @@ public class Program
     public static void main(String[] args) 
     {
         Funcionario empregado , gerente , func;
-        gerente = new Funcionario("Fernando", "Gerente", 2802 , 10 , 8 , 1999 , "RJ-157.033.171");
+        //Inserindo os dados dos funcionarioss
+        gerente = new Funcionario("Fernando", "Gerente", 30000 , 10 , 8 , 1999 , "RJ-157.033.171");
+        empregado = new Funcionario("Kaio", "Caixa", 10000 , 9 , 12  , 2005 , "MG-015.287.478");
         func = gerente;
-        empregado = new Funcionario("Kaio", "Caixa", 2802 , 9 , 12  , 2005 , "MG-015.287.478");
+        //Lendo os dados inseridos
         gerente.DadosFuncionario();
         empregado.DadosFuncionario();
-            if (func == gerente) 
-            {
-                System.out.println("A");
-            }
+        //Aqui comprara se é o mesmo funcionario vendo se esta salvo no mesmo lugar da memoria 
+        if (func == gerente) 
+        {
+            System.out.println("A");
+        }
     }
 }
